0fca78fea2433eab74cb88eced78aae0dfa01559
Mastermind20 <39518698+Mastermind20@users.noreply.github.com>
2018-05-22 13:14:47 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'NDQ1MTIxNjIxMTg4MTQ5MjQ4.DeVt5g.J7uuBMD5FhZDBA_wa6LLOKw5oLs'; //التوكين هنا
+
+exports.PREFIX = 'La'; // البرريفكس
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8'; // لاتعدل هنا شي
